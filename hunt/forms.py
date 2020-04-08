f3afff45956e72c9e416a583bb9a10ac8d038c21
Chuck Weinberger <chuckhw@sbcglobal.net>
2020-04-08 12:48:21 -0700

    improved the look of the signup form

--- hunt/forms.py
+++ hunt/forms.py
@@ -10,12 +10,52 @@ class SignUpForm(UserCreationForm):
                                 label='Team Name',
                                 widget=forms.TextInput(attrs={'autofocus': True, 'class':'form-control'})
                             )
-    captain = forms.CharField(max_length=100, label='Team Captain', help_text='Please use your real name')
-    team_member2 = forms.CharField(max_length=100, label='Team Member 1', required=False, help_text='You can have up to 4 Team Members')
-    team_member3 = forms.CharField(max_length=100, label='Team Member 2', required=False, help_text='You can have up to 4 Team Members')
-    team_member4 = forms.CharField(max_length=100, label='Team Member 3', required=False, help_text='You can have up to 4 Team Members')
-    team_member5 = forms.CharField(max_length=100, label='Team Member 4', required=False, help_text='You can have up to 4 Team Members')
-    email = forms.EmailField(max_length=150, help_text='Team Captain\'s Email')
+    captain = forms.CharField(  max_length=100, 
+                                label='Team Captain', 
+                                help_text='Please use your real name',
+                                widget=forms.TextInput(attrs={'class':'form-control'})
+                              )
+    email = forms.EmailField(
+                                max_length=150, 
+                                help_text='Team Captain\'s Email',
+                                widget=forms.TextInput(attrs={'class':'form-control'})
+                            )
+    password1 = forms.CharField(
+                                    label="Team Password",
+                                    strip=False,
+                                    widget=forms.TextInput(attrs={'type':'password','class':'form-control', 'autocomplete': 'new-password'}),
+                                    help_text=("Choose a password that you can share with your team")
+                                  )
+    password2 = forms.CharField(
+                                     label="Confirm Password",
+                                     strip=False,
+                                     widget=forms.TextInput(attrs={'type':'password', 'class':'form-control', 'autocomplete': 'new-password'}),
+                                     help_text=("Enter the same password as above for verification"),
+                                )
+    team_member2 = forms.CharField( max_length=100, 
+                                    label='Team Member 1', 
+                                    required=False, 
+                                    help_text='You can have up to 4 Team Members',
+                                    widget=forms.TextInput(attrs={'class':'form-control'})
+                                   )
+    team_member3 = forms.CharField( max_length=100, 
+                                    label='Team Member 2', 
+                                    required=False, 
+                                    help_text='You can have up to 4 Team Members',
+                                    widget=forms.TextInput(attrs={'class':'form-control'})
+                                   )
+    team_member4 = forms.CharField( max_length=100, 
+                                    label='Team Member 3', 
+                                    required=False, 
+                                    help_text='You can have up to 4 Team Members',
+                                    widget=forms.TextInput(attrs={'class':'form-control'})
+                                   )
+    team_member5 = forms.CharField( max_length=100, 
+                                    label='Team Member 4', 
+                                    required=False, 
+                                    help_text='You can have up to 4 Team Members',
+                                    widget=forms.TextInput(attrs={'class':'form-control'})
+                                   )
 
 class AnswerForm(forms.Form):
     answer = forms.CharField(label="What\'s your guess?", max_length=400, widget=forms.TextInput(attrs={'class':'form-control'}))
