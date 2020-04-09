929d99f3b088513a150f092656161ebfb75aca4c
Chuck Weinberger <chuckhw@sbcglobal.net>
2020-04-09 11:38:34 -0700

    added an answer model

--- hunt/models.py
+++ hunt/models.py
@@ -5,6 +5,13 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+        
+class Answer(models.Model):
+    answer_text = models.CharField(max_length=400)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    right = models.BooleanField(default=False)
+    made_on = models.DateTimeField('answer date', null=True, blank=True)
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=400)
@@ -13,6 +20,7 @@ class Question(models.Model):
     answer3 = models.CharField(max_length=200, null=True, blank=True)
     times_solved = models.IntegerField(default=0)
     number = models.IntegerField(default=1, unique=True)
+    answers = models.ForeignKey(Answer, blank=True, null=True, on_delete=models.CASCADE)
         
     class Meta:
         ordering = ['number']
--- piratehunt/settings.py
+++ piratehunt/settings.py
@@ -110,7 +110,7 @@ EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = 'jackson_weinberger@cate.org'
+# DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 
 # Password validation
